7f689c48c12abafcaea71e5a9e933f91ea729dd2
Merle Antony <antony.merle@univ-pau.fr>
2021-01-02 18:43:30 +0100

    tuto init

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace TelePrompterConsole
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var lines = ReadFrom("sampleQuotes.txt");
+      StreamReader reader = new StreamReader("sampleQuotes.txt");
+      string[] array = reader.ReadToEnd().Split(' ');
+      // foreach (var line in lines)
+      // {
+      //   Console.WriteLine(line);
+      // }
+
+      StreamWriter writer = new StreamWriter("output.txt");
+      foreach (var mot in array)
+      {
+        writer.WriteLine(mot);
+      }
+    }
+
+    static IEnumerable<string> ReadFrom(string file)
+    {
+      string line;
+      using (var reader = File.OpenText(file))
+      {
+        while ((line = reader.ReadLine()) != null)
+        {
+          yield return line;
+        }
+      }
+    }
+  }
+}
